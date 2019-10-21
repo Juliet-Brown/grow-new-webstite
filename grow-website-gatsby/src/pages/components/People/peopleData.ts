7e58d5dabdbe5f8eeea75a84de1b2501fada830a
catepalmer <cate@springload.co.nz>
2019-10-22 12:45:16 +1300

    Update intro content

--- grow-website-gatsby/src/pages/components/People/peopleData.ts
+++ grow-website-gatsby/src/pages/components/People/peopleData.ts
@@ -13,13 +13,13 @@ const peopleData = [
     name: "Katie Day",
     imageSrc: katieImage,
     description:
-      "Katie started off as a graphic designer, and that background plays a huge part in how she approaches front-end development. She loves working with designers, and is passionate about making the best decisions for the user experience. She learned front- end development at Media Design School in Auckland, and has a passion for building beautiful, scalable UIs with CSS.",
+      "Katie started off as a graphic designer, and that background plays a huge part in how she approaches front-end development. She loves working with designers, and is passionate about making the best decisions for the user experience. She learned front-end development at Media Design School in Auckland, and has a passion for building beautiful, scalable UIs with CSS.",
   },
   {
     name: "Rachel Bolt",
     imageSrc: rachelImage,
     description:
-      "Rachel got her Bachelor of Design with first class honours at Massey University, and joined Springload as a Summer of Tech intern at the end of 2018. She was quickly snapped up for a permanent position. She’s an experience designer who’s passionate about putting people first – using service, user experience, and user interface design to create digital experiences that make a positive impact for people and the planet.",
+      "Rachel got her Bachelor of Design with first class honours at Massey University, and joined Springload as a Summer of Tech intern at the end of 2018. She was quickly snapped up for a permanent position. She’s an experience designer who’s passionate about putting people first — using service, user experience, and user interface design to create digital experiences that make a positive impact for people and the planet.",
   },
   {
     name: "Usha MacDonald",
@@ -36,7 +36,8 @@ const peopleData = [
   {
     name: "Oisín Hassett",
     imageSrc: oisínImage,
-    description: "Oisín, who hails from Ireland, is Springload’s studio and events manager. He started out in the hospitality industry when he was fifteen, and after finishing school worked his way up to management at the Guinness Storehouse in Dublin. That may be a far cry from working at a tech company, but here at Springload he gets to do what he loves most — helping people. From organising food for workshops to getting T-shirts printed for the team, he’s been indispensable in helping run Grow."
+    description:
+      "Oisín, who hails from Ireland, is Springload’s studio and events manager. He started out in the hospitality industry when he was fifteen, and after finishing school worked his way up to management at the Guinness Storehouse in Dublin. That may be a far cry from working at a tech company, but here at Springload he gets to do what he loves most — helping people. From organising food for workshops to getting T-shirts printed for the team, he’s been indispensable in helping run Grow.",
   },
   {
     name: "Gisela de la Villa",
@@ -54,13 +55,13 @@ const peopleData = [
     name: "Cate Palmer",
     imageSrc: cateImage,
     description:
-      "Before training to be a developer at Enspiral Dev Academy, Cate’s craft was fiction writing - she has an MA from the International Institute of Modern Letters, and an MFA from the Michener Center for Writers in Austin, Texas. Now a front-end developer pursuing her writing alongside her tech career, she’s excited to have the chance through Grow to work with people from non-STEM backgrounds who might see tech as an intimidating sector to break into.",
+      "Before training to be a developer at Enspiral Dev Academy, Cate’s craft was fiction writing — she has an MA from the International Institute of Modern Letters, and an MFA from the Michener Center for Writers in Austin, Texas. Now a front-end developer pursuing her writing alongside her tech career, she’s excited to have the chance through Grow to work with people from non-STEM backgrounds who might see tech as an intimidating sector to break into.",
   },
   {
     name: "Juliet Brown",
     imageSrc: julietImage,
     description:
-      "Juliet is a front-end developer and an artist who studied at Elam School of Fine Arts, and then at Enspiral Dev Academy. Not content to leave these two careers in their own separate silos, she’s excited by the challenge of pushing her art and technology together, and is inspired by artist/developers like Sarah Drasner and Zach Lieberman. Outside of her work at Springload, she’s constantly looking for ways to evolve her art - working on digital animations and interactive pieces, and drawing and coding together.",
+      "Juliet is a front-end developer and an artist who studied at Elam School of Fine Arts, and then at Enspiral Dev Academy. Not content to leave these two careers in their own separate silos, she’s excited by the challenge of pushing her art and technology together, and is inspired by artist/developers like Sarah Drasner and Zach Lieberman. Outside of her work at Springload, she’s constantly looking for ways to evolve her art — working on digital animations and interactive pieces, and drawing and coding together.",
   },
 ]
 
