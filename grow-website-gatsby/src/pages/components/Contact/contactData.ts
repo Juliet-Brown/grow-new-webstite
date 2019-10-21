dd75bff7f3abde6c6458aaf966074ad44ec4b966
catepalmer <cate@springload.co.nz>
2019-10-22 10:12:00 +1300

    Extract social media link data into separate file

---
+++ grow-website-gatsby/src/pages/components/Contact/contactData.ts
@@ -0,0 +1,29 @@
+import emailIcon from "../../../images/email.svg"
+import instagramIcon from "../../../images/instagram.svg"
+import phoneIcon from "../../../images/phone.svg"
+import twitterIcon from "../../../images/twitter.svg"
+
+const contactData = [
+  {
+    icon: emailIcon,
+    href: "mailto:grow@springload.co.nz",
+    text: "grow@springload.co.nz",
+  },
+  {
+    icon: phoneIcon,
+    href: "tel:64-4-801-8205",
+    text: "+64 4 801 8205",
+  },
+  {
+    icon: twitterIcon,
+    href: "https://www.twitter.com/growbysl",
+    text: "twitter.com/growbysl",
+  },
+  {
+    icon: instagramIcon,
+    href: "https://www.instagram.com/growbyspringload",
+    text: "instagram.com/growbyspringload",
+  },
+]
+
+export default contactData
