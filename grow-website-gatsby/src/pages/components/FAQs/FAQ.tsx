dc2fdc058c5998cf824e3dccba98278b41f77260
catepalmer <cate@springload.co.nz>
2019-10-22 15:43:33 +1300

    Refactor FAQ into separate component

---
+++ grow-website-gatsby/src/pages/components/FAQs/FAQ.tsx
@@ -0,0 +1,16 @@
+import * from "react"
+import * as styles from "../../Index.module.scss"
+
+const FAQ = (item, i) => {
+  return (
+
+                  <div className={styles.gridItemContainer} key={i}>
+                    <div className={styles.headingSmall}>{item.question}</div>
+                    <div className={styles.paragraphSmall}>
+                      <p>{item.answer}</p>
+                    </div>
+                  </div>
+  )
+}
+
+export default FAQ
--- grow-website-gatsby/src/pages/components/FAQs/FAQs.tsx
+++ grow-website-gatsby/src/pages/components/FAQs/FAQs.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react"
 import * as styles from "../../Index.module.scss"
 import FAQData from "./FAQData"
+import FAQ from "./FAQ"
 
 const FAQs = () => {
   return (
@@ -14,14 +15,7 @@ const FAQs = () => {
             </div>
             <div className={styles.grid}>
               {item.questions.map((item, i) => {
-                return (
-                  <div className={styles.gridItemContainer} key={i}>
-                    <div className={styles.headingSmall}>{item.question}</div>
-                    <div className={styles.paragraphSmall}>
-                      <p>{item.answer}</p>
-                    </div>
-                  </div>
-                )
+                return <FAQ item={item} i={i} />
               })}
             </div>
           </Fragment>
