1c2b8231825514e4ab564b13d78fcf51827c5bda
catepalmer <cate@springload.co.nz>
2019-10-22 17:29:22 +1300

    Revert back to FAQs component with no separate FAQ component

--- grow-website-gatsby/src/pages/components/FAQs/FAQs.tsx
+++ grow-website-gatsby/src/pages/components/FAQs/FAQs.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import * as styles from "../../Index.module.scss"
 import FAQData from "./FAQData"
-import FAQ from "./FAQ"
 
 const FAQs = () => {
   return (
@@ -15,7 +14,14 @@ const FAQs = () => {
             </div>
             <div className={styles.grid}>
               {item.questions.map((item, i) => {
-                return <FAQ item={item} i={i} />
+                return (
+                  <div className={styles.gridItemContainer} key={i}>
+                    <div className={styles.headingSmall}>{item.question}</div>
+                    <div className={styles.paragraphSmall}>
+                      <p>{item.answer}</p>
+                    </div>
+                  </div>
+                )
               })}
             </div>
           </>
