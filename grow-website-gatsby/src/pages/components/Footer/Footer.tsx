2cc0fac076a209c5b0aa1d8edca333aed12abd37
catepalmer <cate@springload.co.nz>
2019-10-23 15:02:04 +1300

    Add social icons to footer

--- grow-website-gatsby/src/pages/components/Footer/Footer.tsx
+++ grow-website-gatsby/src/pages/components/Footer/Footer.tsx
@@ -2,23 +2,45 @@ import * as React from "react"
 import * as styles from "../../Index.module.scss"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import FooterData from "./FooterData"
+import springloadIcon from "../../../images/springload.svg"
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.gutters}>
-        <div className={styles.footerLinkContainer}>
-          {FooterData.map((section, i) => {
-            return (
-              <AnchorLink
-                className={styles.footerLink}
-                href={section.href}
-                key={i}
-              >
-                {section.name}
-              </AnchorLink>
-            )
-          })}
+        <div className={styles.footerContainer}>
+          <div className={styles.footerLinkContainer}>
+            {FooterData.sections.map((section, i) => {
+              return (
+                <AnchorLink
+                  className={styles.footerLink}
+                  href={section.href}
+                  key={i}
+                >
+                  {section.name}
+                </AnchorLink>
+              )
+            })}
+          </div>
+          <div className={`${styles.footerLinkContainer} ${styles.footerIconContainer}`}>
+            <div className={`${styles.flex} ${styles.footerSocialLinkContainer}`}>
+              {FooterData.socialIcons.map((icon, i) => {
+                return (
+                  <a
+                    className={styles.footerIcon}
+                    target="_blank"
+                    href={icon.href}
+                    key={i}
+                  >
+                    <img src={icon.imageSrc} />
+                  </a>
+                )
+              })}
+            </div>
+            <div className={styles.footerIcon}>
+              <img src={springloadIcon} />
+            </div>
+          </div>
         </div>
       </div>
     </footer>
--- grow-website-gatsby/src/pages/components/Footer/FooterData.tsx
+++ grow-website-gatsby/src/pages/components/Footer/FooterData.tsx
@@ -1,24 +1,39 @@
-const FooterData = [
-  {
-    name: "About",
-    href: "#about",
-  },
-  {
-    name: "Workshops",
-    href: "#workshop",
-  },
-  {
-    name: "People",
-    href: "#people",
-  },
-  {
-    name: "FAQs",
-    href: "#faqs",
-  },
-  {
-    name: "Contact",
-    href: "#contact",
-  },
-]
+import instagramIcon from "../../../images/instagram.svg"
+import twitterIcon from "../../../images/twitter.svg"
+
+const FooterData = {
+  sections: [
+    {
+      name: "About",
+      href: "#about",
+    },
+    {
+      name: "Workshops",
+      href: "#workshop",
+    },
+    {
+      name: "People",
+      href: "#people",
+    },
+    {
+      name: "FAQs",
+      href: "#faqs",
+    },
+    {
+      name: "Contact",
+      href: "#contact",
+    },
+  ],
+  socialIcons: [
+    {
+      imageSrc: twitterIcon,
+      href: "https://www.twitter.com/growbysl",
+    },
+    {
+      imageSrc: instagramIcon,
+      href: "https://www.instagram.com/growbyspringload",
+    },
+  ],
+}
 
 export default FooterData
