4cc59ee5eda5ffb83c81ffd2629e4af5926c55e7
Juliet Brown <juliet.brown@springload.co.nz>
2020-08-03 16:04:19 +1200

    update grow banner image

--- src/pages/components/Banner/Banner.tsx
+++ src/pages/components/Banner/Banner.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as styles from "../../Index.module.scss"
-import growBannerImage from "../../../images/grow-banner.jpg"
+import growBannerImage from "../../../images/team-image.jpg"
 
 const Banner = () => {
   return (
