f4505e21805e0928a2dc6b6edb2a74a558cf9dca
Cate Palmer <catherinepalmer@gmail.com>
2021-01-18 17:39:15 +1300

    Add description for Grow step-by-step guide

--- src/pages/components/Resources/Resources.tsx
+++ src/pages/components/Resources/Resources.tsx
@@ -1,12 +1,49 @@
 import * as React from "react"
 import * as styles from "../../Index.module.scss"
 
+const ResourcesData = {
+  description: [
+    {
+      one:
+        "Whether or not you’ve attended one of our workshops, we’ve created these learning resources to help you on your coding journey.",
+    },
+  ],
+  resources: [
+    {
+      title: "Grow workshop step-by-step guide",
+      description:
+        "This step-by-step guide takes you through the coding exercise we do in our three-hour workshop. If you’ve already attended a workshop, consider it a roadmap to what we did, which you can work through again at your own pace. Usually three hours is not enough time to fully understand everything or ingrain what you learned, so going over the material again can be hugely beneficial. But even if you haven’t attended a workshop, this guide is designed to stand alone so that anyone who’s interested in learning to code can work through it. And if you haven’t attended a workshop yet but want to, we’d highly recommend it as preparation!",
+    },
+  ],
+}
+
 const Resources = () => {
   return (
     <div className={styles.gutters}>
       <h2 className={styles.uMarginBottomSM} id="resources">
         Resources
       </h2>
+      <div className={`${styles.uWidthSixtyPercent} ${styles.uMarginBottomSM}`}>
+        {ResourcesData.description.map((paragraph, i) => {
+          return (
+            <div key={i}>
+              <p className={styles.paragraphSmall}>{paragraph.one}</p>
+            </div>
+          )
+        })}
+      </div>
+      {ResourcesData.resources.map((resource, i) => {
+        return (
+          <div key={i} className={styles.grid}>
+            <div className={styles.gridItemImageContainer} key={i}>
+              <div className={styles.headingMedium}>{resource.title}</div>
+              <div className={styles.paragraphSmall}>
+                <p>{resource.description} </p>
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
