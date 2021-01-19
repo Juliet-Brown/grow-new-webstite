0891561b0996f1353e3c2e30fb34f3591d68067a
Cate Palmer <catherinepalmer@gmail.com>
2021-01-20 10:48:33 +1300

    Add step-by-step guide as a pdf

--- src/pages/components/Resources/Resources.tsx
+++ src/pages/components/Resources/Resources.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 import * as styles from "../../Index.module.scss"
+import stepByStepGuide from "../../../static/step-by-step-guide.pdf"
+import pdfIcon from "../../../images/pdf-icon.png"
 
 const ResourcesData = {
   description: [
@@ -13,6 +15,9 @@ const ResourcesData = {
       title: "Grow workshop step-by-step guide",
       description:
         "This step-by-step guide takes you through the coding exercise we do in our three-hour workshop. If you’ve already attended a workshop, consider it a roadmap to what we did, which you can work through again at your own pace. Usually three hours is not enough time to fully understand everything or ingrain what you learned, so going over the material again can be hugely beneficial. But even if you haven’t attended a workshop, this guide is designed to stand alone so that anyone who’s interested in learning to code can work through it. And if you haven’t attended a workshop yet but want to, we’d highly recommend it as preparation!",
+      linkIcon: pdfIcon,
+      linkTitle: "Grow step-by-step guide",
+      linkUrl: stepByStepGuide
     },
   ],
 }
@@ -38,7 +43,11 @@ const Resources = () => {
             <div className={styles.gridItemImageContainer} key={i}>
               <div className={styles.headingMedium}>{resource.title}</div>
               <div className={styles.paragraphSmall}>
-                <p>{resource.description} </p>
+                <p className={styles.uMarginBottomSM}>{resource.description}</p>
+                <a className={styles.linkResource} href={resource.linkUrl}>
+                  <img className={styles.linkIcon} src={resource.linkIcon}></img>
+                  <p>{resource.linkTitle}</p>
+                </a>
               </div>
             </div>
           </div>
